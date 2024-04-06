7c5d267c40cd9ef4c09839139bcbd7effae47311
Andrew Nzioki <anzioki99@gmail.com>
2024-04-07 00:16:13 +0300

    Added the individualRoomsScreen

---
+++ app/src/main/java/com/homey/homeysystemsappv10/NavScreens/SpacesSetup/IndividualRooms/IndividualRoomsScreen.kt
@@ -0,0 +1,103 @@
+package com.homey.homeysystemsappv10.NavScreens.SpacesSetup.IndividualRooms
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.homey.homeysystemsappv10.NavScreens.BuildingScreen.BackButton
+import com.homey.homeysystemsappv10.NavScreens.BuildingScreen.TopScreenBar
+import com.homey.homeysystemsappv10.R
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun individualRoomsScreen() {
+
+    val numbers = listOf(1, 2, 3, 4, 5)
+    Scaffold(
+        topBar =
+        {
+            TopScreenBar(
+                text = "",
+                leftAction = { BackButton(onClick = {/*TODO*/ }) }
+
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = innerPadding.calculateTopPadding(),
+                    start = 16.dp,
+                    end = 16.dp
+                )
+        ) {
+            genericCard(
+                title = "Sockets"
+            ) {
+                LazyColumn(
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    items(numbers) { item ->
+                        Card {
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable { /*TODO*/ }
+                            ) {
+                                Text(
+                                    " Switch $item",
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                )
+                                IconButton(
+                                    onClick = { /*TODO*/ } // Invoke the onClick lambda
+                                ) {
+                                    Image(
+                                        painterResource(R.drawable.backicon),
+                                        contentDescription = "BackButton"
+                                    )
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun genericCard(
+    title: String = "",
+    content: @Composable () -> Unit = {}
+) {
+    Text(
+        title,
+        style = TextStyle(fontSize = 24.sp)
+    )
+    Spacer(
+        modifier = Modifier
+            .height(4.dp)
+    )
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        content()
+    }
+}
